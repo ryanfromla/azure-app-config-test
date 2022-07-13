61b1f85672ade1e12f6082b18339464cb4d74290
Ryan Peterson <ryan.peterson@tapcheck.com>
2022-07-13 16:17:05 -0700

    change to configvalue

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,18 @@ namespace config_test.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IConfiguration _configuration;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
+        _configuration = configuration;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
-        return View();
+        return Content(_configuration.GetValue<string>("ConfigValue"), "text/plain");
     }
 
     public IActionResult Privacy()
